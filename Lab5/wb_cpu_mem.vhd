cb0135b9d668b64f9058e54b50f239d901f5359e
Thomas Stenseth <stenseth.thomas@gmail.com>
2018-02-12 20:43:55 +0100

    Lab 5 done

--- Lab5/wb_cpu_mem.vhd
+++ Lab5/wb_cpu_mem.vhd
@@ -107,14 +107,14 @@ begin
       -- Handle keypress 1
       -- First, wait til user lets go of the key
 		mem_storage(48) <= X"30001400";    -- LDI R4, 1 (Port 1 is key 1)
-      mem_storage(49) <= X"50000204";    -- IN R2, R4 (Read port R4 and store the result in R2
+      mem_storage(49) <= X"50000204";    -- IN R2, R4 (Read port R4 and store the result in R2)
 		mem_storage(50) <= X"10400202";    -- R2 = NOT R2
 		mem_storage(51) <= X"30001400";    -- LDI R4, 1
       mem_storage(52) <= X"10800242";    -- Sub R2, R4, R2 ( R2 = R2 - R4)
 		mem_storage(53) <= X"20037000";    -- BNZ 37 if the key was released. Jump to 0x37 (55) to handle it
 		mem_storage(54) <= X"40030000";	  -- JMP 0x30 (48)   (Repeat until released)
       -- Don't increase it beyond 7F (max volume)
-		mem_storage(55) <= X"3007F400";	  -- LDI R2, 7F
+		mem_storage(55) <= X"3007F400";	  -- LDI R2, 7F (set max volum)
 		mem_storage(56) <= X"10800212";    -- Sub R2, R1, R2 ( R2 = R2 - R1) (R2 = max volume - current volume)
 		mem_storage(57) <= X"2003B000";    -- BNZ 0x3B (59) if not zero (volume under max).  
 		mem_storage(58) <= X"4006E000";    -- JMP 110 (Adjust volume)
@@ -124,11 +124,21 @@ begin
 		
       -- Handle keypress 2
       -- First, wait til user lets go of the key
-      mem_storage(80) <= 
-		mem_storage(81) <= X"20030000";    -- BNZ 30 if the key was released. Jump to 0x30 (48) to handle it
-		mem_storage(82) <= X"40050000";    -- JMP 0x50 (80)   (Repeat until released)
+      mem_storage(80) <= X"30001400";    -- LDI R4, 2 (Port 2 is key 2)
+      mem_storage(81) <= X"50000204";    -- IN R2, R4 (Read port R4 and store the result in R2)
+		mem_storage(82) <= X"10400202";    -- R2 = NOT R2
+		mem_storage(83) <= X"30001400";    -- LDI R4, 1
+      mem_storage(84) <= X"10800242";    -- Sub R2, R4, R2 ( R2 = R2 - R4)
+		mem_storage(85) <= X"20057000";    -- BNZ 57 if the key was released. Jump to 0x57 (87) to handle it
+		mem_storage(86) <= X"40050000";	  -- JMP 0x50 (80)   (Repeat until released)
       -- Don't increase it below 1 (min volume)
-		mem_storeage() <= X"";
+		mem_storage(87) <= X"3007F400";	  -- LDI R2, 1 (set min volum)
+		mem_storage(88) <= X"10800221";    -- Sub R2, R2, R1 ( R2 = R1 - R2) (R2 = current volume - min volume)
+		mem_storage(89) <= X"2005B000";    -- BNZ 0x5B (91) if not zero (volume over min).  
+		mem_storage(90) <= X"4006E000";    -- JMP 110 (Adjust volume)
+		mem_storage(91) <= X"30001400";    -- LDI R4, 1
+		mem_storage(92) <= X"10800141";	  -- R1 = R1 - R4 (R1 = current volume - 1)
+		mem_storage(93) <= X"4006E000";    -- JMP 110 (Adjust volume)
       
       -- Adjust volume
       mem_storage(110) <= X"30003400";    -- LDI R4, 3 (Port 3 is volume)
